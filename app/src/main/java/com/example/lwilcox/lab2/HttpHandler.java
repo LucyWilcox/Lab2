cdf2a890417064d7773ccdd0c03b6269d5726c66
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-09-23 16:22:51 -0400

    Can get thumbnails! Can't actually see them...

--- app/src/main/java/com/example/lwilcox/lab2/HttpHandler.java
+++ app/src/main/java/com/example/lwilcox/lab2/HttpHandler.java
@@ -11,6 +11,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -26,7 +27,7 @@ public class HttpHandler {
     public void imageSearch(String searchQuery, final Callback callback) {
         String query = searchQuery.replaceAll(" ", "+");
         String URL = "https://www.googleapis.com/customsearch/v1?cx=015805936300530222953:xfn9wvqvajy&key=AIzaSyBHSXnNE-tEICkyVFO_dgktm1sLbmXxwPw&";
-        URL = URL + "q=" + searchQuery + "%searchType=image";
+        URL = URL + "q=" + searchQuery + "&searchType=image";
 
         JsonObjectRequest getRequest = new JsonObjectRequest(
                 Request.Method.GET,
@@ -37,16 +38,17 @@ public class HttpHandler {
                     public void onResponse(JSONObject response){
                         ArrayList<String> displayImages = new ArrayList<String>();
                         try {
-                            JSONObject items = response.getJSONObject("Items");
-                            JSONObject image = response.getJSONObject("image");
+                            JSONArray items = response.getJSONArray("items");
                             for (int i = 0; i < items.length(); i++) {
+                                JSONObject item = items.getJSONObject(i);
+                                JSONObject image = item.getJSONObject("image");
                                 String thumbnailImage = image.getString("thumbnailLink");
                                 displayImages.add(thumbnailImage);
                             }
                                 callback.callback(displayImages);
                         } catch (Exception e)
                         {
-                            Log.d("Success", "whoo");
+                            Log.d("Failure", "Not extracting thumbnail from JSON Object");
                         }
                     }
                 },
--- app/src/main/java/com/example/lwilcox/lab2/SearchFragment.java
+++ app/src/main/java/com/example/lwilcox/lab2/SearchFragment.java
@@ -24,6 +24,8 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -36,9 +38,12 @@ public class SearchFragment extends Fragment {
     }
 
     public Button searchButton;
+    public  Button nextButton;
+    public Button previousButton;
     private View myFragmentView;
     public EditText editText;
     public ImageView imageView;
+    public Integer photoIndex;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,32 +51,54 @@ public class SearchFragment extends Fragment {
         myFragmentView = inflater.inflate(R.layout.fragment_search, container, false);
 
         searchButton = (Button) myFragmentView.findViewById(R.id.search);
+        nextButton = (Button) myFragmentView.findViewById(R.id.next);
+        previousButton = (Button) myFragmentView.findViewById(R.id.previous);
         editText = (EditText) myFragmentView.findViewById(R.id.editText);
         imageView = (ImageView) myFragmentView.findViewById(R.id.imageView);
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String searchText = editText.getText().toString();
                 HttpHandler handler = new HttpHandler(getActivity().getApplicationContext());
+                photoIndex = 0;
                 handler.imageSearch(searchText, new Callback() {
                     @Override
                     public void callback(ArrayList<String> displayList) {
-                        String thumbnailLink = displayList.get(0);
-                        try {
-                            URL url = new URL(thumbnailLink);
-                            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                            imageView.setImageBitmap(bmp);
-                        } catch (Exception e)
-                        {
-                            Log.d("Error", "String not converting to URL");
-                        }
+                        String thumbnailLink = displayList.get(photoIndex);
+                        setImage(thumbnailLink);
                     }
                 });
-
             }
         });
-
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                photoIndex += 1;
+            }
+        });
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                photoIndex -= 1;
+            }
+        });
         return myFragmentView;
+    }
+
+    public void setImage(String thumbnailLink){
+        try {
+            URL url = new URL(thumbnailLink);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input;
+            input = connection.getInputStream();
+            Bitmap bmp = BitmapFactory.decodeStream(input);
+            imageView.setImageBitmap(bmp);
+        } catch (Exception e) {
+            Log.d("Error", "String not converting to URL");
+        }
     }
 }
 
